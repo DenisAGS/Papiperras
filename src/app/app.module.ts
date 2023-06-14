cb75f6858c1be2f86d7ccfc47be0e1f3d8d7f455
DenisAGS <denissearelyg@gmail.com>
2023-06-14 16:07:54 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,72 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+/*import { MatIconModule} from '@angular/material/icon';*/
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing-module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrincipalComponent } from './principal/principal.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { DevolucionComponent } from './devolucion/devolucion.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { PreguntasFrecuentesComponent } from './preguntas-frecuentes/preguntas-frecuentes.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { HeaderPageComponent } from './header-page/header-page.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { Publicidad2Component } from './publicidad/publicidad2/publicidad2.component';
+import { PubOfertasComponent } from './ofertas/pub-ofertas/pub-ofertas.component';
+import { PublicarPComponent } from './publicar-productos/publicar-p/publicar-p.component' ;
+import { ReclamosFormularioComponent } from './reclamos-formulario/reclamos-formulario.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RetiroComponent } from './retiro/retiro.component';
+import { DatosRetiroComponent } from './datos-retiro/datos-retiro.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SaldosComponent } from './saldos/saldos.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarioComponent } from './calendario/calendario.component';
+import { EstadisticaComponent } from './estadistica/estadistica.component';
+import { VolumenComponent } from './volumen/volumen.component';
+import { ConsejosComponent } from './consejos/consejos.component';
+import { FooterPageComponent } from './footer-page/footer-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    AboutUsComponent,
+    DevolucionComponent,
+    InventarioComponent,
+    PreguntasFrecuentesComponent,
+    TutorialComponent,
+    HeaderPageComponent,
+    SidebarComponent,
+    Publicidad2Component,
+    PubOfertasComponent,
+    PublicarPComponent,
+    ReclamosFormularioComponent,
+    HomepageComponent,
+    RetiroComponent,
+    DatosRetiroComponent,
+    FooterPageComponent,
+    EstadisticaComponent,
+    ConsejosComponent,
+    CalendarioComponent,
+    VolumenComponent,
+    SaldosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    FullCalendarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
